f257fe9a6d13c5c40c97ea47296e33e26f0ed05f
Madhura G <madhura7us@gmail.com>
2019-03-13 12:05:39 -0700

    Main source code

---
+++ DeepLearning_ImgProc/main.py
@@ -0,0 +1,108 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+print("Group members: Madhura Umesh Gajendragadkar, Akshay Jayaraj, Vasanta Surya Roop Teja")
+print("Ubit names: mgajendr, akshayja, vasantas")
+print("Ubit names: 50246666, 50214185, 50250075")
+
+
+# In[ ]:
+
+PATH="E:/Project_4/img_align_celeba/"
+data = np.genfromtxt('list_attr_celeba.txt', delimiter='',dtype=int, names=True)
+attributes= data['Eyeglasses']
+PATH1="E:/Project_4/Specs/"
+PATH2="E:/Project_4/No_Specs/"
+for j in range(1,50000):
+    img1=cv2.imread(PATH+str(j)+".jpg")
+    if(attributes[j]==1):
+        cv2.imwrite(os.path.join(PATH1+str(j)+".jpg"),img1)
+    elif(attributes[j]==-1):
+        cv2.imwrite(os.path.join(PATH2+str(j)+".jpg"),img1)
+
+
+# In[20]:
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+
+
+# dimensions of our images.
+img_width, img_height = 150, 150
+
+train_data_dir = 'E:/Project_4/data/train'
+validation_data_dir = 'E:/Project_4/data/validation'
+nb_train_samples = 500
+nb_validation_samples = 150
+epochs = 50
+batch_size = 16
+
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
+
+model = Sequential()
+model.add(Conv2D(64, (3, 3), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(128, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.25))
+model.add(Dense(1))
+model.add(Activation('relu'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+# this is the augmentation configuration for training:
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+
+# this is the augmentation configuration for testing:
+# only rescaling
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+validation_generator = test_datagen.flow_from_directory(
+    validation_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_validation_samples // batch_size)
+
+
+# In[ ]:
+
+
+
